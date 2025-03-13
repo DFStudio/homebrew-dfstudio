36be491f0db5d0f6bf7894c47d5ef64a798bb936
Kate Corcoran <kate.corcoran@dfstudio.com>
2025-03-13 12:23:49 -0700

    Update dynamodb-daemon

--- Formula/dynamodb-daemon.rb
+++ Formula/dynamodb-daemon.rb
@@ -1,21 +1,23 @@
 class DynamodbDaemon < Formula
   desc "Locally-hosted DynamoDB server"
   homepage "https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/DynamoDBLocal.html"
-  #url "https://dynamodb-local.s3.amazonaws.com/dynamodb_local_latest.tar.gz"
-  url "https://raw.githubusercontent.com/DFStudio/homebrew-dfstudio/artifacts/dynamodb-daemon/DynamoDBLocal_complete-1.18.0.tar.gz"
-  version "1.18.0"
-  sha256 "628d9a3c413c9328239def1277e7722b25f0cbe36ce045b3e0c3f41b445c418e"
+  #url "https://d1ni2b6xgvw0s0.cloudfront.net/v2.x/dynamodb_local_latest.tar.gz"
+  url "https://raw.githubusercontent.com/DFStudio/homebrew-dfstudio/artifacts/dynamodb-daemon/dynamodb_local_2.5.4.tar.gz"
+  version "2.5.4"
+  revision 4
+  sha256 "60b1561fa6141642dbd3ad9ab75a6315c94877f6f42e60551125b11ea8a62c97"
   #license ""
 
-  # depends_on "openjdk"
+  depends_on "openjdk@17"
 
   def install
     libexec.install "DynamoDBLocal_lib"
-    libexec.install "jre"
+    libexec.install "DynamoDBLocal.jar"
+
     (bin/"dynamodb-local").write <<~EOS
       #!/bin/bash
-      export JAVA_HOME="#{libexec}/jre"
-      exec "${JAVA_HOME}/bin/java" -Djava.library.path='#{libexec}/DynamoDBLocal_lib' -classpath '#{libexec}/DynamoDBLocal_lib/*' com.amazonaws.services.dynamodbv2.local.main.ServerRunner "$@"
+      export JAVA_HOME="#{Language::Java.overridable_java_home_env("17")[:JAVA_HOME]}"
+      exec "${JAVA_HOME}/bin/java" -Djava.library.path='#{libexec}/DynamoDBLocal_lib' -jar '#{libexec}/DynamoDBLocal.jar' "$@"
     EOS
   end
 
@@ -37,7 +39,7 @@ class DynamodbDaemon < Formula
   end
 
   service do
-    run [bin/"dynamodb-local", "--port", "8800", "--dbPath", f.dynamodb_db_path]
+    run [bin/"dynamodb-local", "-port", "8800", "-dbPath", f.dynamodb_db_path, "-cors", "https://dynamodb-manager.netlify.app"]
     keep_alive true
     log_path f.dynamodb_log_path
     error_log_path f.dynamodb_log_path
