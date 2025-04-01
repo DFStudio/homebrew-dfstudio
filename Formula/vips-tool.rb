81db72c80eca7b061e280552a34b1793ec920ce7
Kate Corcoran <kate.corcoran@dfstudio.com>
2025-04-01 14:42:54 -0700

    Upgrade vips-tool to 1.1.2

--- Formula/vips-tool.rb
+++ Formula/vips-tool.rb
@@ -1,10 +1,10 @@
 class VipsTool < Formula
   desc "A general-purpose vips pipeline tool"
   homepage ""
-  url "https://github.com/DFStudio/c-vips-scale/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "6fdd7c5c7e3ce3111bc59dc02f6fab004f90363158eaa1a9b4365d2b2ac01644"
+  url "https://github.com/DFStudio/c-vips-scale/archive/refs/tags/v1.1.2.tar.gz"
+  sha256 "dc2460be134a3cef3adab7b79be57a36ffc2fe7be9e0eb250b45789aebbba84c"
   license ""
-  version "1.1.1"
+  version "1.1.2"
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
@@ -18,18 +18,5 @@ class VipsTool < Formula
     system "cmake", ".", *std_cmake_args
     system "cmake", "--build", ".", "--target", "all"
     bin.install "vips-tool"
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test vips-tool`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
-    system "false"
   end
 end
